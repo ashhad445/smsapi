be6c459aba4930044b469a8d2a1c4b64da7420c4
ashhad445 <bscs2312377@szabist.pk>
2025-05-13 10:34:18 +0000

    message

--- studentWEB/js/countscript.js
+++ studentWEB/js/countscript.js
@@ -25,3 +25,18 @@ fetch(COUNT_COUNTRY).then(response=>{
 }).catch(err=>{
     console.lock(err.message);
 });
+
+const COUNT_DEPARTMENTS = "https://organic-bassoon-v6wjx7rg5rxv3w4pq-5001.app.github.dev/count_departments";
+fetch(COUNT_DEPARTMENTS).then(response=>{
+    if(!response.ok)
+        throw new Error("Failed to fetch Data");
+    return response.json();
+}).then(data=>{
+   
+    
+    data.forEach(coun=>{
+        document.getElementById("Countries").innerHTML = ` Total Departments  ${coun.count}`;
+    });
+}).catch(err=>{
+    console.lock(err.message);
+});
